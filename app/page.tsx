d45285e3865a33d0b148816de2da7fd2fb5232f5
Dona <dona.snow.studio7@gmail.com>
2025-03-16 15:42:56 +0200

    Add AccordionSection component and update HeroSection layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import AccordionSection from "./components/sect/AccordionSection";
 import HeroSection from "./components/sect/HeroSection";
 export default function Home() {
   return (
     <main>
       <HeroSection />
+
+      <AccordionSection />
     </main>
   );
 }
